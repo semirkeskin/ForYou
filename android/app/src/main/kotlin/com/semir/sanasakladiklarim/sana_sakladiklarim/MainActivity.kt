757904fc96d642cc1d219af500ed617df87fc72b
semir <semirkeskin01@gmail.com>
2026-05-16 19:26:28 +0300

    feat: Picture-in-Picture (PiP) + multi-window destek
    Tablet'te uygulama arka plana atildiginda (home tusu) otomatik kucuk
    floating pencereye gecer. Pencere ekranda surukleyebilir, sabit kalir.
    Tiklayinca tam ekran doner.
    
    Degisiklikler:
    - AndroidManifest: resizeableActivity='true' + supportsPictureInPicture='true'
      (Xiaomi MIUI mini-window de bu sayede aktif olur)
    - MainActivity.kt: onUserLeaveHint override - home tusunda otomatik
      enterPictureInPictureMode (9:16 portrait, API 26+)
    
    Xiaomi Pad 7'de iki kullanim:
    1) Home tusu -> otomatik PiP (Android standart, surukleyebilir)
    2) Gorev gecis menusu -> 'Mini pencere' (MIUI ozelligi, tam interaktif,
       surukleyebilir + boyutlandirilabilir)

--- android/app/src/main/kotlin/com/semir/sanasakladiklarim/sana_sakladiklarim/MainActivity.kt
+++ android/app/src/main/kotlin/com/semir/sanasakladiklarim/sana_sakladiklarim/MainActivity.kt
@@ -1,5 +1,29 @@
 package com.semir.sanasakladiklarim.sana_sakladiklarim
 
+import android.app.PictureInPictureParams
+import android.os.Build
+import android.util.Rational
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+
+    /**
+     * Home tusuna basildiginda otomatik olarak Picture-in-Picture moduna gec.
+     * Boylece uygulama arka plana atildiginda kucuk surukleyebilir pencere
+     * olarak ekranda kalir. Android 8 (API 26) ve uzeri destekler.
+     */
+    override fun onUserLeaveHint() {
+        super.onUserLeaveHint()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O &&
+            packageManager.hasSystemFeature(
+                android.content.pm.PackageManager.FEATURE_PICTURE_IN_PICTURE
+            )
+        ) {
+            // 9:16 portrait oraninda kucuk pencere
+            val params = PictureInPictureParams.Builder()
+                .setAspectRatio(Rational(9, 16))
+                .build()
+            runCatching { enterPictureInPictureMode(params) }
+        }
+    }
+}
